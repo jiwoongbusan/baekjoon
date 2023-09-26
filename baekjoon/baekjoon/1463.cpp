8636d09dabcd76136dea89bd259827252f25da32
jiwoong5 <127164088+jiwoong5@users.noreply.github.com>
2023-09-26 19:31:47 +0900

    1463

---
+++ baekjoon/baekjoon/1463.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int result[1000001];
+
+int cal_min(int n) {
+	result[1] = 0;
+	result[2] = 1;
+	result[3] = 1;
+	for (int i = 4; i <= n; i++) {
+		result[i] = result[i - 1] + 1;
+		if (i % 3 == 0) result[i] = min(result[i], result[i / 3] + 1);
+		if (i % 2 == 0) result[i] = min(result[i], result[i / 2] + 1);
+	}
+	return result[n];
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout<<cal_min(n);
+	return 0;
+}
